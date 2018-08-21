ccf0a38dc9c9ab3236a2c3918b0ba58f95cf4c6c
Marten Stockenberg <MStockenberg@googlemail.com>
2018-08-21 13:16:10 +0200

    removed typo

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-g
+
 require_once('app/configs/main.php');
 require_once('app/configs/secret.php');
 require_once("vendor/autoload.php");
